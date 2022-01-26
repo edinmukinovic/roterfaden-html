51057950b187d2eb11fbf6cf4a0cc8f957f02cad
edinmukinovic <97086405+edinmukinovic@users.noreply.github.com>
2022-01-26 11:25:17 +0100

    Delete includes directory

--- includes/layout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function getLayout($layoutName = false, $data = false) {
-        if ($layoutName) {
-            $data = $data;
-            include(__DIR__ . "/../layouts/$layoutName.php");
-        }
-    }
-?>
